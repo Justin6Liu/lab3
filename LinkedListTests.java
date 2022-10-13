fd9d83eaf90db498cf2fea6863c83573945914fa
Justin6Liu <114266776+Justin6Liu@users.noreply.github.com>
2022-10-13 15:49:29 -0700

    Wrote tests for list and fixed bugs in list

---
+++ LinkedListTests.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import java.lang.reflect.Array;
+import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class LinkedListTests {
+    
+}
---
+++ ListTests.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.*;
+import java.lang.reflect.Array;
+import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTests{
+    @Test
+    public void testForFilter(){
+        List<String> input1 = new ArrayList<>();
+        input1.add("a");
+        input1.add("b");
+        input1.add("c");
+        // false output should be c,b,a
+        assertEquals(input1, ListExamples.filter(input1, new AlwaysCorrectChecker()));
+    }
+
+    @Test
+    public void testForMerge(){
+        List<String> input1 = new ArrayList<>();
+        input1.add("a");
+        input1.add("c");
+        input1.add("e");
+        List<String> input2 = new ArrayList<>();
+        input2.add("b");
+        input2.add("d");
+        input2.add("f");
+        List<String> correct = new ArrayList<>();
+        String[] sl = new String[]{"a","b","c","d","e","f"};
+        for(String s:sl){correct.add(s);}
+        assertEquals(correct, ListExamples.merge(input1,input2));
+
+    }
+}
